60c2e71ba76ea1e7369cdeb97a0a153260eaa22e
lemfritz11 <aintdaiki@gmail.com>
2024-09-13 22:14:35 +0800

    initial commit with number.js

---
+++ number.js
@@ -0,0 +1,16 @@
+let firstFavNum = 11;
+let secondFavNum = 4;
+
+let addition = firstFavNum + secondFavNum;
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulus = firstFavNum % secondFavNum;
+let exponentiation = firstFavNum ** secondFavNum;
+
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponentiation);
